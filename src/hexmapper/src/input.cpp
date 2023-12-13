abfa87275dedf36dc16e818e420b1618b8addeb0
Andrzej Drabarek <andrzej.drabarek@gmail.com>
2023-12-13 12:43:17 +0100

    Viewport scrolling with middle mouse button.

--- src/hexmapper/src/input.cpp
+++ src/hexmapper/src/input.cpp
@@ -1,18 +1,35 @@
 #include "input.h"
 
 void CollectInput(Input *input) {
-    constexpr f32 cameraSpeed = 10.0f;
     input->cameraShift = (Vector2){0, 0};
-    if (IsKeyDown(KEY_W)) {
-        input->cameraShift.y -= cameraSpeed;
+    {  // Camera shift - keyboard
+        f32 cameraSpeed = 10.0f;
+
+        if (IsKeyDown(KEY_LEFT_CONTROL)) {
+            cameraSpeed *= 0.1f;
+        } else if (IsKeyDown(KEY_LEFT_SHIFT)) {
+            cameraSpeed *= 2.0f;
+        }
+
+        if (IsKeyDown(KEY_W)) {
+            input->cameraShift.y -= cameraSpeed;
+        }
+        if (IsKeyDown(KEY_S)) {
+            input->cameraShift.y += cameraSpeed;
+        }
+        if (IsKeyDown(KEY_A)) {
+            input->cameraShift.x -= cameraSpeed;
+        }
+        if (IsKeyDown(KEY_D)) {
+            input->cameraShift.x += cameraSpeed;
+        }
     }
-    if (IsKeyDown(KEY_S)) {
-        input->cameraShift.y += cameraSpeed;
-    }
-    if (IsKeyDown(KEY_A)) {
-        input->cameraShift.x -= cameraSpeed;
-    }
-    if (IsKeyDown(KEY_D)) {
-        input->cameraShift.x += cameraSpeed;
+
+    {  // Camera shift - mouse
+        if (IsMouseButtonDown(MOUSE_MIDDLE_BUTTON)) {
+            Vector2 mouseDelta = GetMouseDelta();
+            input->cameraShift.x -= mouseDelta.x;
+            input->cameraShift.y -= mouseDelta.y;
+        }
     }
 }
